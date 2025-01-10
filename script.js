cf0da2d18e82ba31e477adb4bc4e614f78dc96d6
Naufal4rtcrew <84627124+NaufalCyber4rtCrew@users.noreply.github.com>
2025-01-10 15:44:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Ayah";
+  sender = "Han";  // Ganti Han dengan Ayah
 }
 
 let footer = document.getElementById("credit");
